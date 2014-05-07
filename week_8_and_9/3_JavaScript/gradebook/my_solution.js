d73e54e362b9a120ef189777abc8e7ec20d89e1d
SamuelDavis <sam9@cfu.net>
2014-05-07 16:34:24 -0400

    uploading

--- week_8_and_9/3_JavaScript/gradebook/my_solution.js
+++ week_8_and_9/3_JavaScript/gradebook/my_solution.js
@@ -7,7 +7,7 @@ variables match (i.e., 'Joseph' is the first element in students; his scores are
 
 Do not alter the students and scores code.
 
-I worked on this challenge [by myself, with:]
+I worked on this challenge by myself.
 
 */
 
@@ -26,31 +26,174 @@ var scores = [ [80, 70, 70, 100],
 // __________________________________________
 // Write your code below.
 
+// function average(array_scores)
+// {
+//   var i, sum = 0, avg = 0;
+
+//   for (i = 0; i < array_scores.length; i++)
+//       sum += array_scores[i];
+
+//   avg = (sum/array_scores.length);
+
+//   return avg;
+// }
 
 
+// tester to make sure that I found the avg correctly
+// average([1,2,3]);
+
+//function gradebook(name, scores)
+//{
+  //this.name = name;
+  //this.scores = scores;
+//}
+
+// var gradebook = {
+//   Joseph: {
+//     testScores: scores[0]
+//   },
+
+//   Susan: {
+//     testScores: scores[1]
+//   },
+
+//   William: {
+//     testScores: scores[2]
+//   },
+
+//   Elizabeth: {
+//     testScores: scores[3]
+//   },
+
+//   addScore: function(name, score)
+//   {
+//     var i; 
+
+//     for (i = 0; i < students.length; i++)
+//       if (name === students[i])
+//         return scores[i].push(score);
+//   },
+
+//   getAverage: function(name)
+//   {
+//     var i;
+
+//     for (i = 0; i < students.length; i++)
+//       if (name === students[i])
+//         return average(scores[i]);
+//   }
+// };
+
+// var joseph = new gradebook(students[0], scores[0]);
+// var susan = new gradebook(students[1], scores[1]);
+// var william = new gradebook(students[2], scores[2]);
+// var elizabeth = new gradebook(students[3], scores[3]);
+
+// var people = new Array(joseph, susan, william, elizabeth);
+
+// function printgradeBook(people)
+// {
+//   var i;
+
+//   for (i = 0; i < people.length; i++)
+//   {
+//       console.log(people[i].name + " - " + people[i].scores);
+//   }
+// }
+
+// printgradeBook(people);
 
 
 
 // __________________________________________
 // Refactored Solution
 
+// I went through a ton of refactoring when I was writing the 
+// initial solution. This was an awesome experience kind of 
+// relearning everything, since it had been awhile since I 
+// had done it.
 
 
+function average(array_scores)
+{
+  var i, sum = 0, avg = 0;
 
+  for (i = 0; i < array_scores.length; i++)
+      sum += array_scores[i];
 
+  avg = (sum/array_scores.length);
 
+  return avg;
+}
 
+var gradebook = {
+  Joseph: {
+    testScores: scores[0]
+  },
+
+  Susan: {
+    testScores: scores[1]
+  },
+
+  William: {
+    testScores: scores[2]
+  },
+
+  Elizabeth: {
+    testScores: scores[3]
+  },
+
+  addScore: function(name, score)
+  {
+    var i; 
+
+    for (i = 0; i < students.length; i++)
+      if (name === students[i])
+        return scores[i].push(score);
+  },
+
+  getAverage: function(name)
+  {
+    var i;
+
+    for (i = 0; i < students.length; i++)
+      if (name === students[i])
+        return average(scores[i]);
+  }
+};
 
 // __________________________________________
 // Reflect
+What parts of your strategy worked? What problems did you face?
 
+Well, when writing the first function, I ran into very little problems.
+My problems came when I tried doing the gradebook. I ran into a lot
+of syntax errors from not programming in javascript in awhile.
 
+What questions did you have while coding? What resources did you find to help you answer them?
 
+I had some syntax questions. I referred to my notes and previous challenges.
 
+What concepts are you having trouble with, or did you just figure something out? If so, what?
 
+I wasn't really having any problems with concepts. Just needed 
+to refresh my memory. 
 
+Did you learn any new skills or tricks?
 
+Not really. Just got some great practice. 
 
+How confident are you with each of the Learning Competencies?
+
+I am very confident. Really felt like I knew what was going on. 
+
+Which parts of the challenge did you enjoy?
+
+Everything.
+
+Which parts of the challenge did you find tedious?
+
+Nothing. 
 
 // __________________________________________
 // Driver Code:  Do not alter code below this line.

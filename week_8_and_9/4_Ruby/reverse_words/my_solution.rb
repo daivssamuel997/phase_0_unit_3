48e304bb5f1ada370274acee9c3cfa6dffb8b4ab
SamuelDavis <sam9@cfu.net>
2014-05-12 20:36:56 -0700

    practice

--- week_8_and_9/4_Ruby/reverse_words/my_solution.rb
+++ week_8_and_9/4_Ruby/reverse_words/my_solution.rb
@@ -9,21 +9,34 @@
 
 # 3. Initial Solution
 
-
+# def reverse_words(word)
+# 	word = word.split(" ")
+# 	word.map!{ |x| x.reverse }.join(" ")
+# end
 
 
 # 4. Refactored Solution
 
+def reverse_words(word)
 
+	word.gsub(/\w+/) { |match| match.reverse }
 
-
+end
 
 
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
+def assert 
+	raise "Assertion failed!" unless yield
+end
 
+assert { reverse_words("") == "" }
 
+assert { reverse_words("hello") == "olleh" }
 
+assert { reverse_words("hello dog") == "olleh god" }
+
+assert { reverse_words("Ich bin ein Berliner") == "hcI nib nie renilreB" }
 
 
 # 5. Reflection 
